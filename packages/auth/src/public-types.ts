e577156598e02faec4291e385132e205bf32a0e0
Malte Hallström <malte.hallstrom@gmail.com>
2022-06-27 20:12:58 +0200

    refactor: remove built-in QR code generation

--- packages/auth/src/public-types.ts
+++ packages/auth/src/public-types.ts
@@ -1,13 +1,10 @@
 export type DeviceFlowParams = {
-  /** The url used to sign in manually  */
+  /** The url used to sign in manually (url excl. code)  */
   verification_uri: string;
-  /** The url used to sign in automatically  */
+  /** The url used to sign in automatically (url incl.code) */
   verification_uri_complete: string;
   /** The code that can be used to sign in manually  */
   user_code: string;
-  /** The QR code to scan in order to open the
-   * verification_uri_complete URL */
-  qr: QrObject;
 };
 
 /** A callback that will be called with the initial parameters,
@@ -30,14 +27,6 @@ export interface User {
 }
 
 export type Listener = (user: User | null) => void;
-
-export interface QrObject {
-  /** The raw buffer */
-  buffer: Buffer;
-  /** A string that be directly used within an <img> tag.
-   *  Includes the buffer in base64 form. */
-  imgSrc: string;
-}
 
 /** Unsubscribe function stop receiving updates. */
 export type Unsubscribe = () => void;
