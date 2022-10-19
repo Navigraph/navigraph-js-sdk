72fe845c18f615ed3d3706d775ca35ba2a2f0a1e
Malte Hallström <malte.hallstrom@gmail.com>
2022-10-19 18:14:32 +0200

    fix: call token revocation endpoint on logout

--- packages/auth/src/constants.ts
+++ packages/auth/src/constants.ts
@@ -1,3 +1,4 @@
 export const IDENTITY_ROOT = "https://identity.api.navigraph.com";
 export const IDENTITY_DEVICE_AUTH = IDENTITY_ROOT + "/connect/deviceauthorization";
 export const IDENTITY_ENDSESSION_ENDPOINT = IDENTITY_ROOT + "/connect/endsession";
+export const IDENTITY_REVOCATION_ENDPOINT = IDENTITY_ROOT + "/connect/revocation";
