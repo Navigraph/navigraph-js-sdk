7c6e0d3c35986a96c669fe22117cbef75723d7c9
Malte Hallström <malte.hallstrom@gmail.com>
2022-10-30 21:12:59 +0100

    fix: improve init process of auth module

--- examples/getting-started/src/hooks/useNavigraphAuth.tsx
+++ examples/getting-started/src/hooks/useNavigraphAuth.tsx
@@ -37,12 +37,12 @@ function useProvideAuth() {
   // component that utilizes this hook to re-render with the latest auth object.
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((u) => {
-      if (!isInitialized) setIsInitialized(true);
       setUser(u);
+      if (!isInitialized) setIsInitialized(true);
     });
     // Cleanup subscription on unmount
     return () => unsubscribe();
-  }, [isInitialized]);
+  }, []);
 
   return {
     user,
