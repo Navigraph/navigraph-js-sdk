ae782238a109ba370711dcb66d896a7636fc7e3a
Malte Hallström <malte.hallstrom@gmail.com>
2023-03-28 23:26:00 +0200

    build: include deps in bundle, use ES6

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   clean: true,
   format: ["cjs", "esm"],
-  target: "es5",
-  outExtension(ctx) {
-    return { js: `.${ctx.format}.js` };
-  },
+  target: "es6",
+  noExternal: ["axios", "@navigraph/pkce"],
+  outExtension: (ctx) => ({ js: `.${ctx.format}.js` }),
 });
