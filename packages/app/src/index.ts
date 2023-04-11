c181c89faeb35ba6b51465ed4201bc1ec143a379
Malte Hallström <malte.hallstrom@gmail.com>
2023-04-11 17:43:33 +0200

    refactor: restructure app module

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -1,4 +1,5 @@
-export * from "./public-types";
-export * from "./api";
-export * from "./errors";
+export * from "./types";
+export * from "./shared/errors";
 export { default as Logger } from "./lib/Logger";
+export { default as initializeApp, type NavigraphApp } from "./lib/initializeApp";
+export { getApp } from "./internals/apps";
---
+++ packages/app/src/types.ts
@@ -0,0 +1,8 @@
+export enum Scope {
+  "CHARTS" = "charts",
+  "FMSDATA" = "fmsdata",
+  "EMAIL" = "email",
+  "TILES" = "tiles",
+  /** @deprecated Included by default, no need to include it explicitly */
+  "OFFLINE" = "offline_access",
+}
