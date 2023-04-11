4d9df5dc2c9d55d8b328cc4402279e96cba1e2c6
Malte Hallström <malte.hallstrom@gmail.com>
2023-04-11 18:00:22 +0200

    chore: add missing export

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -2,4 +2,4 @@ export * from "./types";
 export * from "./shared/errors";
 export { default as Logger } from "./lib/Logger";
 export { default as initializeApp, type NavigraphApp } from "./lib/initializeApp";
-export { getApp } from "./internals/apps";
+export { getApp, getDefaultAppDomain } from "./internals/apps";
