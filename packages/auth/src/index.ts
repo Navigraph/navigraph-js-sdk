65fa41ecb558a987bb32ee627002e8f42d98aa68
Malte Hallström <malte.hallstrom@gmail.com>
2023-05-18 21:30:44 +0200

    refactor: stop exposing internal types

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,4 +1,4 @@
-export * from "./lib/navigraphRequest";
+export { CancelToken, navigraphRequest, isAxiosError } from "./lib/navigraphRequest";
 export { default as getAuth } from "./lib/getAuth";
 export type * from "./flows/device-flow";
 export type { User, UserCallback, Unsubscribe } from "./internals/user";
