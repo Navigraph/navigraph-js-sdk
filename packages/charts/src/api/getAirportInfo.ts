d4512f862c05d50da31ac0406078235758e05919
Malte Hallström <malte.hallstrom@gmail.com>
2023-07-27 23:57:28 +0200

    fix: use correct base URL for airport info

--- packages/charts/src/api/getAirportInfo.ts
+++ packages/charts/src/api/getAirportInfo.ts
@@ -1,6 +1,6 @@
 import { Logger } from "@navigraph/app";
-import { navigraphRequest, isAxiosError } from "@navigraph/auth";
-import { getChartsApiRoot } from "../constants";
+import { isAxiosError, navigraphRequest } from "@navigraph/auth";
+import { getAirportApiRoot } from "../constants";
 import { AirportInfo } from "./types";
 
 /** Fetches general, useful information about an airport such as fuel types, repairs, and more
@@ -9,7 +9,7 @@ import { AirportInfo } from "./types";
  */
 export default async function getAirportInfo({ icao }: { icao: string }): Promise<AirportInfo | null> {
   const result = await navigraphRequest
-    .get<AirportInfo>(`${getChartsApiRoot()}/${icao}/airport`)
+    .get<AirportInfo>(`${getAirportApiRoot()}/${icao}`)
     .catch((e: unknown) =>
       Logger.err("Failed to fetch airport information. Reason:", isAxiosError(e) ? e.message : e)
     );
