400cf17cd3a89b5577825141ec5c00e07a913aa9
Malte Hallström <malte.hallstrom@gmail.com>
2023-08-13 18:54:27 +0200

    feat: add `NavigraphTileLayer` for use w/ Leaflet

--- packages/charts/src/index.ts
+++ packages/charts/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./api/types";
 export * from "./api/chartTypeCodes";
 export * from "./util";
+export { default as NavigraphTileLayer } from "./lib/NavigraphTileLayer";
 
 export { getChartsAPI } from "./lib/getChartsAPI";
---
+++ packages/charts/src/lib/NavigraphTileLayer.ts
@@ -0,0 +1,107 @@
+import { Logger, getDefaultAppDomain } from "@navigraph/app";
+import { NavigraphAuth } from "@navigraph/auth";
+import { TileLayer, Coords, DoneCallback } from "leaflet";
+
+enum RasterSource {
+  "IFR HIGH" = "ifr.hi",
+  "IFR LOW" = "ifr.lo",
+  "VFR" = "vfr",
+  "WORLD" = "world",
+}
+
+type RasterTheme = "DAY" | "NIGHT";
+
+function getNavigraphTileURL(
+  source: keyof typeof RasterSource = "VFR",
+  theme: RasterTheme = "DAY",
+  retina = false
+) {
+  return `https://enroute-bitmap.charts.api-v2.${getDefaultAppDomain()}/styles/${RasterSource[source]}.${theme.toLowerCase()}/{z}/{x}/{y}${retina ? "@2x" : "{r}"}.png` // prettier-ignore
+}
+
+export interface PresetConfig {
+  source: keyof typeof RasterSource;
+  theme: RasterTheme;
+  forceRetina?: boolean;
+}
+
+/**
+ * A Leaflet tile layer that renders Navigraph enroute charts.
+ * @example
+ * ```ts
+ * const navigraphLayer = new NavigraphTileLayer(auth, { source: "IFR HIGH", theme: "NIGHT" });
+ * navigraphLayer.addTo(map);
+ *
+ * navigraphLayer.setPreset({ source: "IFR LOW", theme: "DAY" });
+ * ```
+ */
+export default class NavigraphTileLayer extends TileLayer {
+  /** A list of tiles that has failed to load since the last successful tile load. */
+  protected FAILED_TILES = new Set<string>();
+
+  /** Indicates whether map tiles failed to load due to authentication being invalid or missing. */
+  private isMissingAuth = false;
+
+  constructor(public auth: NavigraphAuth, public preset: PresetConfig) {
+    super(getNavigraphTileURL(preset.source, preset.theme, preset.forceRetina));
+
+    auth.onAuthStateChanged((user) => {
+      if (this.isMissingAuth && user) {
+        this.redraw();
+        this.isMissingAuth = false;
+      }
+    });
+
+    if (!this.auth.isInitialized()) {
+      Logger.warning(
+        "NavigraphLayer was created before Navigraph Auth was initialized. Tiles may fail to load until a user is signed in."
+      );
+    }
+  }
+
+  /**
+   * Changes the preset that the map is rendering. Automatically rerenders the map.
+   * @param preset The base style of the map tiles.
+   * @param theme The color theme of the map tiles.
+   * @example
+   * ```ts
+   * navigraphLayer.setPreset({ source: "IFR HIGH", theme: "NIGHT" });
+   * ```
+   */
+  public setPreset(preset: PresetConfig) {
+    this.preset = preset;
+    const newUrl = getNavigraphTileURL(preset.source, preset.theme, preset.forceRetina);
+    this.setUrl(newUrl);
+  }
+
+  protected createTile(coords: Coords, done: DoneCallback): HTMLElement {
+    const url = this.getTileUrl(coords);
+    const img = document.createElement("img");
+
+    img.onerror = () => {
+      Logger.debug("Failed to load tile!");
+
+      this.isMissingAuth = this.auth.getUser() === null;
+      const tileHasFailedBefore = this.FAILED_TILES.has(url);
+
+      if (tileHasFailedBefore || this.isMissingAuth) return;
+
+      Logger.debug("Refreshing auth and tile...");
+      this.FAILED_TILES.add(url);
+      this.auth
+        .getUser(true)
+        .then(() => (img.src = url))
+        .catch(() => (this.isMissingAuth = true));
+    };
+
+    img.onload = () => {
+      done(undefined, img);
+      this.FAILED_TILES.clear();
+      Logger.debug("Loaded tile successfully!");
+    };
+
+    img.src = url;
+
+    return img;
+  }
+}
