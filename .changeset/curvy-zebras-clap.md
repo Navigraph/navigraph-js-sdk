23db6a601d77c12e2d7be5b2381c2d19af667fa8
Malte Hallström <malte.hallstrom@gmail.com>
2023-08-14 16:49:02 +0200

    refactor: make leaflet layer separate module

---
+++ .changeset/curvy-zebras-clap.md
@@ -0,0 +1,7 @@
+---
+"@navigraph/charts": major
+---
+
+Moved the Leaflet integration to a separate package.
+
+You can now find the `NavigraphTileLayer` in `@navigraph/leaflet`!
--- packages/charts/src/index.ts
+++ packages/charts/src/index.ts
@@ -1,6 +1,5 @@
 export * from "./api/types";
 export * from "./api/chartTypeCodes";
 export * from "./util";
-export * from "./lib/NavigraphTileLayer";
 
 export { getChartsAPI } from "./lib/getChartsAPI";
