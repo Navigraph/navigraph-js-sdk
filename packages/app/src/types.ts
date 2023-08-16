596489472608b75190ed5e969638c1c3a76eecdc
Malte Hallström <malte.hallstrom@gmail.com>
2023-08-17 00:38:38 +0200

    chore: remove email scope

--- packages/app/src/types.ts
+++ packages/app/src/types.ts
@@ -1,7 +1,6 @@
 export enum Scope {
   "CHARTS" = "charts",
   "FMSDATA" = "fmsdata",
-  "EMAIL" = "email",
   "TILES" = "tiles",
   /** @deprecated Included by default, no need to include it explicitly */
   "OFFLINE" = "offline_access",
