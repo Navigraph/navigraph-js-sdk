3f9f1dbeaf15afa26d29d62d8dee14590d57be84
Malte Hallström <malte.hallstrom@gmail.com>
2023-08-17 01:10:48 +0200

    feat: add ability to change chart index version

--- packages/charts/src/api/getChartImage.ts
+++ packages/charts/src/api/getChartImage.ts
@@ -2,19 +2,18 @@ import { Logger } from "@navigraph/app";
 import { isAxiosError, navigraphRequest } from "@navigraph/auth";
 import { Chart } from "./types";
 
+interface ChartImageRequestParams {
+  chart: Chart;
+  /** @default "light" */
+  theme?: "light" | "dark";
+}
+
 /** Fetches a chart image blob based on a {@link Chart} object.
  * @param {Chart} options.chart - The chart relevant Chart object
  * @param {("light"|"dark")} options.theme - The theme to apply to the chart
  * @returns {Blob} A chart image blob
  */
-export default async function getChartImage({
-  chart,
-  theme = "light",
-}: {
-  chart: Chart;
-  /** @default "light" */
-  theme?: "light" | "dark";
-}): Promise<Blob | null> {
+export default async function getChartImage({ chart, theme = "light" }: ChartImageRequestParams) {
   const imageUrl = theme === "light" ? chart.image_day_url : chart.image_night_url;
   const result = await navigraphRequest
     .get<Blob>(imageUrl, {
--- packages/charts/src/api/getChartsIndex.ts
+++ packages/charts/src/api/getChartsIndex.ts
@@ -1,15 +1,21 @@
 import { Logger } from "@navigraph/app";
 import { navigraphRequest, isAxiosError } from "@navigraph/auth";
 import { getChartsApiRoot } from "../constants";
-import { Chart, ChartsIndexResponse } from "./types";
+import { ChartsIndexResponse } from "./types";
+
+interface ChartsIndexRequestParams {
+  icao: string;
+  /** @default "STD" */
+  version?: "CAO" | "STD";
+}
 
 /** Fetches an index of available charts for a specified airport
  * @param options.icao - The ICAO code of an airport
  * @returns {Chart[]} A list of chart objects
  */
-export default async function getChartsIndex({ icao }: { icao: string }): Promise<Chart[] | null> {
+export default async function getChartsIndex({ icao, version = "STD" }: ChartsIndexRequestParams) {
   const result = await navigraphRequest
-    .get<ChartsIndexResponse>(`${getChartsApiRoot()}/${icao}`)
+    .get<ChartsIndexResponse>(`${getChartsApiRoot()}/${icao}`, { params: { version } })
     .catch((e: unknown) =>
       Logger.err("Failed to fetch charts index. Reason:", isAxiosError(e) ? e.message : e)
     );
