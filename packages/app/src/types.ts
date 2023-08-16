acf5d955fc01c68367760e54427be680f53a2c15
Malte Hallström <malte.hallstrom@gmail.com>
2023-08-17 01:05:56 +0200

    chore: remove deprecated scope

--- packages/app/src/types.ts
+++ packages/app/src/types.ts
@@ -2,6 +2,4 @@ export enum Scope {
   "CHARTS" = "charts",
   "FMSDATA" = "fmsdata",
   "TILES" = "tiles",
-  /** @deprecated Included by default, no need to include it explicitly */
-  "OFFLINE" = "offline_access",
 }
