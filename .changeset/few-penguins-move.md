cdd26ca46021c0fb252686b5ab939280300c6697
Malte Hallström <malte.hallstrom@gmail.com>
2023-09-09 00:39:21 +0200

    fix: include finally polyfill in dist output

---
+++ .changeset/few-penguins-move.md
@@ -0,0 +1,5 @@
+---
+"@navigraph/auth": patch
+---
+
+Included a polyfill for `Promise.finally` to make the SDK work in MSFS directly out of the box without additional setup.
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsup"
 
 export default defineConfig({
   clean: true,
   format: ["cjs", "esm"],
   target: "es6",
   noExternal: ["axios", "@navigraph/pkce"],
-  outExtension: (ctx) => ({ js: `.${ctx.format}.js` }),
-});
+  outExtension: ctx => ({ js: `.${ctx.format}.js` }),
+})
