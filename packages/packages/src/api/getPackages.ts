56bd92f3007fa2e71e76e3b10fc8928522c50a2c
Markus Hamburger <markus@grandnoise.se>
2023-11-14 03:45:43 +0100

    refactor: adds default option for developers with several packages

--- packages/packages/src/api/getPackages.ts
+++ packages/packages/src/api/getPackages.ts
@@ -3,7 +3,7 @@ import { isAxiosError, navigraphRequest } from "@navigraph/auth"
 import { getPackagesApiRoot } from "../constants"
 import { NavigraphPackage, PackageStatus } from "./types"
 
-export interface PackageResponseItem {
+interface PackageResponseItem {
   package_id: string
   cycle: string
   revision: string
@@ -26,10 +26,12 @@ export interface PackageResponseItem {
  * Fetches Navigraph packages from the API.
  * This private shared function can retrieve either a single package or an array of packages based on the provided options.
  * It allows specifying the format of the package data and whether a single package should be returned.
+ * It can also be used to fetch the default package.
  * It also maps the API response to the simpler NavigraphPackage type to avoid exposing the legacy API response structure.
  *
  * @param {Object} [options] - Optional parameters for the fetch request.
  * @param {string} [options.format] - The format of the package data to be returned. If not specified, all available formats are returned.
+ * @param {boolean} [options.single] - Set to true to return only the first package. If false or not provided, an array of packages is returned.
  * @param {boolean} [options.single] - Set to true to return only the first package. If false or not provided, an array of packages is returned.
  * @returns {Promise<NavigraphPackage[] | NavigraphPackage | null>} A promise that resolves to either an array of NavigraphPackage objects, a single NavigraphPackage object, or null if no packages are found.
  * @throws {RequestFailedError} If the API request fails, with a message indicating the reason for failure.
@@ -45,12 +47,19 @@ export interface PackageResponseItem {
 export async function getPackages<
   TSingle extends boolean = false,
   TReturn = TSingle extends true ? NavigraphPackage : NavigraphPackage[],
->(options?: { format?: string; single?: TSingle }): Promise<TReturn | null> {
+>(options?: { format?: string; single?: TSingle; default?: TSingle }): Promise<TReturn | null> {
   try {
-    const formatQuery = options?.format ? `?format=${options.format}` : ""
-    const result = await navigraphRequest.get<PackageResponseItem[]>(`${getPackagesApiRoot()}${formatQuery}`)
+    const queryParams = [
+      options?.format ? `format=${options.format}` : null,
+      options?.default ? "defaultOnly=true" : null,
+    ]
+      .filter(Boolean)
+      .join("&")
 
-    if (options?.single) {
+    const result = await navigraphRequest.get<PackageResponseItem[]>(
+      `${getPackagesApiRoot()}${queryParams ? `?${queryParams}` : ""}`,
+    )
+    if (options?.single || options?.default) {
       return result?.data && result?.data.length > 0 ? (mapResponseToNavigraphPackage(result.data[0]) as TReturn) : null
     } else {
       return result?.data ? (result.data.map(mapResponseToNavigraphPackage) as TReturn) : null
