551b77d0b228ce3883f49592b7f8810f6629ebc3
Markus Hamburger <markus@grandnoise.se>
2023-11-14 03:49:07 +0100

    refactor: formats are unique so removed format option from listPackages as its weird to return an array with only one package.

--- packages/packages/src/api/listPackages.ts
+++ packages/packages/src/api/listPackages.ts
@@ -4,26 +4,18 @@ import { NavigraphPackage } from "./types"
 
 /**
  * Retrieves a list of Navigraph packages.
- * This function fetches packages from the Navigraph API based on the specified format.
- * It's useful for obtaining multiple packages in a specified format or all available formats if none is specified.
+ * It's useful for listing all available packages assigned to the developer.
  *
- * @param {Object} [options] - Optional parameters for the fetch request.
- * @param {string} [options.format] - The format(s) of the package data to be returned, specified as comma-separated values.
- *                                   If not provided, packages in all available formats for your client are returned.
  * @returns {Promise<NavigraphPackage[]>} A promise that resolves to an array of NavigraphPackage objects.
  * @throws {NoPackagesFoundError} If no package is found or if the API request fails.
  * @throws {RequestFailedError} If the API request fails.
  *
  * @example
- * // Fetch packages in a specific format
- * getPackages({ format: 'pdf' }).then(packages => console.log(packages));
- *
- * @example
  * // Fetch packages in all available formats
- * getPackages().then(packages => console.log(packages));
+ * listPackages().then(packages => console.log(packages));
  */
-export async function listPackages(options?: { format: string }): Promise<NavigraphPackage[]> {
-  const packages = await getPackages(options)
+export async function listPackages(): Promise<NavigraphPackage[]> {
+  const packages = await getPackages()
   if (!packages) throw new NoPackagesFoundError()
   return packages
 }
