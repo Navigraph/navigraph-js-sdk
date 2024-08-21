6e3833aeeb57dbee856a38bcc28ea4ebe4f3b082
Malte Hallström <malte.hallstrom@gmail.com>
2024-08-21 13:35:33 +0200

    chore: remove unnecessary word

--- packages/amdb/src/api/searchAmdb.ts
+++ packages/amdb/src/api/searchAmdb.ts
@@ -7,7 +7,7 @@ import { AerodromeReferencePoint, AmdbSearchResult } from "../types"
 /**
  * Searches for airports with AMDB data based on a query string and returns some metadata about them
  *
- * @param query - All AMDB airports whose `idarpt`, `iata`, or `name` fields (from {@link AerodromeReferencePoint}) which **start** with this query string will be returned
+ * @param query - All AMDB airports whose `idarpt`, `iata`, or `name` fields (from {@link AerodromeReferencePoint}) **start** with this query string will be returned
  *
  * @returns A selection of properties from the {@link AerodromeReferencePoint} of the found airports, as well as the coordinates of said reference point
  */
