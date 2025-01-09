a6eab8a00665c77de3f8df9616602e0bc09d7b1e
Alex Cutforth <alex@densem.net>
2025-01-09 09:53:31 +0000

    fix: missing centroid/midpoint fields

--- packages/amdb/src/types/features/types.ts
+++ packages/amdb/src/types/features/types.ts
@@ -1,18 +1,24 @@
 import { Feature, LineString, Point, Polygon } from "geojson"
 import { FeatureType } from "../enums"
 
-type Properties<F extends FeatureType, P> = {
-  /** Unique identifier for this feature instance. Only unique per feature type */
-  id: number
+export type AmdbFeature<P, F extends FeatureType, G extends Point | LineString | Polygon> = Feature<
+  G,
+  {
+    /** Unique identifier for this feature instance. Only unique per feature type */
+    id: number
 
-  feattype: F
+    feattype: F
 
-  /**
-   * ICAO aerodrome location indicator
-   *
-   * Example: `NZCH`
-   */
-  idarpt: string
-} & P
-
-export type AmdbFeature<P, F extends FeatureType, G extends Point | LineString | Polygon> = Feature<G, Properties<F, P>>
+    /**
+     * ICAO aerodrome location indicator
+     *
+     * Example: `NZCH`
+     */
+    idarpt: string
+  } & P &
+    (G extends Polygon
+      ? { centroid: Point }
+      : G extends LineString
+      ? { midpoint: F extends FeatureType.AsrnEdge ? undefined : Point } // This AsrnEdge check isn't very nice, but its neccessary since it is the only table without a populated midpoint value
+      : Record<string, never>)
+>
